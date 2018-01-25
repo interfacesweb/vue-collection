a606f1abdc4461f1cc9701024a38b3fb54898875
Pedro Prieto <pedpral@gmail.com>
2018-01-25 17:14:18 +0100

    App component

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,84 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld @get="getData"/>
-  </div>
+<div id="app">
+  <header>
+    <h1>Cliente Biblioteca Multimedia</h1>
+    
+    <!-- Display collection title -->
+    <h2 class="text-center">{{c.collection.title}}</h2>
+  </header>
+  
+  <main>
+    <!--  links Container  -->
+    <!-- We pass the links array to the links component -->
+    <CollectionLinks :links="c.collection.links" @link-clicked="readCollection"></CollectionLinks>
+    
+    <!--  items Container  -->
+    <!-- We pass the items array to the items component -->
+    <CollectionItems :items="c.collection.items"></CollectionItems>
+    
+    <!-- Template Container -->
+    <!-- We pass collection.href and collection.template to the component -->
+    <CollectionTemplate :createurl="c.collection.href" :template="c.collection.template"></CollectionTemplate>
+  </main>
+</div>
 </template>
 
 <script>
+// Module to make AJAX calls to API server
 import axios from 'axios';
-import HelloWorld from './components/HelloWorld'
+import CollectionLinks from './components/CollectionLinks';
+import CollectionItems from './components/CollectionItems';
+import CollectionTemplate from './components/CollectionTemplate';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  
+  // Initialization:
+  // Function that runs when the component is created
+	// When the app ploads, it connects to the entry point of the API, "/api/", to get a collection object
+	// The collection object is stored in "c"
+  created: function() {
+    // AJAX request to /api/ (entry point of the app)
+    this.readCollection('/api/');
   },
+  
+  // Component data
+  data: function() {
+    return {
+      // This variable will store the Collection + JSON object returned by the server
+      c: null
+    }
+  },
+  
+  // Components defined in this component
+  components: {
+    CollectionItems,
+    CollectionLinks,
+    CollectionTemplate
+  },
+  
+  // Methods
   methods: {
-    getData: function() {
-      console.log('test2');
-      axios.get('/api/')
+    readCollection: function(url) {
+	    // Method to do a GET request to a URL
+	    // It must be called when the user clicks on a LINK
+	    // url: URL of the selected link (the "href" property of the link)
+	    // http://amundsen.com/media-types/collection/format/#general
+      axios.get(url)
         .then(response => {
-          // JSON responses are automatically parsed.
-          console.log(response.data);
-          // this.posts = response.data
+          // If success, store data returned by the server (the collection + JSON object) in the 'c' property of the component
+          this.c = response.data;
         })
         .catch(e => {
+          // If error, display in console
           console.log(e);
-          // this.errors.push(e)
-        })
+        });
+      
     }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
